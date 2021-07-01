418ac74d327621f42f627ae72e5b34a7e5dc6225
aviadhahami <aviad.hahami@snyk.io>
2021-07-01 19:07:03 +0300

    progressing, soon to be done

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const axios = require('axios');
 
 
 const WOLT_EPS = {
-	getLocationId: 'https://restaurant-api.wolt.com/v1/google/places/autocomplete/json?input='
+	getLocationId: 'https://restaurant-api.wolt.com/v1/google/places/autocomplete/json?input=',
+	getLocationGeo: 'https://restaurant-api.wolt.com/v1/google/geocode/json?place_id=',
+	getRestaurants: 'https://restaurant-api.wolt.com/v1/pages/restaurants?'
 }
+const getRestsURLBuilder = (lat, lon) => `${WOLT_EPS.getRestaurants}lat=${lat}&lon=${lon}`;
 
 async function run(){
 
@@ -22,7 +25,18 @@ async function run(){
 	// 1. Get the location ID from wolt
 	const woltLocations = await axios.get(`${WOLT_EPS.getLocationId}${encodeURIComponent(argv._[0])}`); 
 	const woltLocationId = woltLocations.data.predictions[0].place_id; // This is why we only use single arg with no verifications. Feel free to PR this <3
-	return woltLocationId
+	
+	// 2. Get coords from location
+	const woltGeoResponse = await axios.get(`${WOLT_EPS.getLocationGeo}${woltLocationId}`);
+
+	// Coords shape is {lat: number, lng: number}. Yes, no TS.
+	const coords = woltGeoResponse.data.results[0].geometry.location;
+
+	const nearbyRests = (await axios.get(getRestsURLBuilder(coords.lat, coords.lng)))
+											.data
+											.sections[0] // not sure, wolt api :shrug:
+											.items;
+	return nearbyRests.filter(v=>v.venue.online);
 }
 
 run().then(console.log).catch(console.error);
